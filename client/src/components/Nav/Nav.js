dfdacf39336ca714854d8c2838563f09086c2cf6
unknown <mendez.andrewm@gmail.com>
2020-01-13 00:47:13 -0700

    added directories

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+import "./Nav.css";
+
+const Nav = props => {
+
+}
+export default Nav;
---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,5 @@
+const Profile = props => {
+
+}
+
+export default Profile;
