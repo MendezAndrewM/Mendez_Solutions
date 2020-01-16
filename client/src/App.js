1515a6daeeb260b31504256f7dc481327ad18d7e
unknown <mendez.andrewm@gmail.com>
2020-01-15 21:58:21 -0700

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from './logo.svg'; //Not in use
+import { render } from 'react-dom';  //who dis?
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import page from "./pages" // Fix
+import Home from "./pages/Home" //Use until above is fixed
+
+
+const App = props => {
+    const [signedIn, setAuth] = useState(0);
+
+    return (
+        <Router>
+            <div>
+                {/* <Nav loggedIn={this.state.isSignedIn} /> */}
+                <Route exact path="/" component={Home} />
+                {/* until pages export is fixed, must reference pages as done with Home above instead of page.thing like below */}
+                <Route exact path="/profile" component={page.Profile} /> 
+                <Route exact path="/Contact" component={page.Contact} />
+                {/* <Footer /> */}
+            </div>
+        </Router>
+    )
+    
 }
+
 
 export default App;
