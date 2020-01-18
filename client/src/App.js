7dd34007543f1d6f07f5703f147fb965f078ef7d
unknown <mendez.andrewm@gmail.com>
2020-01-17 22:17:57 -0700

    Added starter css

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg'; //Not in use
 import { render } from 'react-dom';  //who dis?
 import './App.css';
-
-import Page from "./pages" // Fix
-import Home from "./pages/Home" //Use until above is fixed
-//
-//
-//            Look up 'render' attribute for react router
-//
+import page from "./pages";
 
 
 const App = props => {
@@ -19,12 +13,9 @@ const App = props => {
         <Router>
             <div>
                 {/* <Nav loggedIn={this.state.isSignedIn} /> */}
-                {/* <Route exact path="/" component={Home} render={() => <Page.Home />} />  */}
-                <Route exact path="/" component={Page.Home} />
-                {/* <Route exact path="/"><Page.Home /></Route> */}
-                {/* until pages export is fixed, must reference pages as done with Home above instead of page.thing like below */}
-                <Route exact path="/Contact" component={Page.Contact} />
-                <Route exact path="/profile" component={Page.Profile} /> 
+                <Route exact path="/" component={page.Home} />
+                <Route exact path="/Contact" component={page.Contact} />
+                <Route exact path="/profile" component={page.Profile} /> 
                 {/* <Footer /> */}
             </div>
         </Router>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -32,22 +32,6 @@ const Home = props => {
                 </Col>
             </Row>
 
-
-            <div id="start" class="row">
-                <div class="s8 m6 offset-s2" id="title">
-                    <h2>Andrew Mendez</h2>
-                    <p>Full Stack Developer. Here to Develop Your Things</p>
-                </div>
-                <div class="s8 m4" id="start-btns">
-                    <button id="profile-btn">See Profile <i class="material-icons tiny">forward</i></button>
-                    <button id="portfolio-btn">Explore Work <i class="material-icons tiny">forward</i></button>
-                </div>
-            </div>
-
-
-
-
-
         </Container>
     </>)
 }
