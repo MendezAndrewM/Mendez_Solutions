46cdb005ec9c9a97ba87d78cd4db68c1c1e51463
Andrew Mendez <mendez.andrewm@gmail.com>
2020-01-24 14:25:57 -0700

    Updated Landing Page to MVP standard
    Updated styles, colors, and responsiveness. Also cleaned contridictions between main.scss and Home.scss. Light restructuring may be needed when adding the animation to the landing page.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,21 +10,29 @@ const Home = props => {
         {/* 1.Need Nav built 2.might not be using hooks right 3.null might be wrong(maybe just empty tag?)*/}
         <Container>
 
-            <Row>
-                <Col xs={{span:10, offset:1}} md={6} lg={5} xl={4} id="Title">
+            <Row className="justify-content-xs-center justify-content-lg-start">
+                <Col xs={12} md={8} lg={6} xl={6} id="Title">
                     <Row>
-                        <h1 id="Header">Mendez Solutions</h1>
-                    </Row>
-                    <Row>
-                        <h4 id="subTitle">A place where your problems get solved</h4>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <button className="HomeButton" >About</button>
+                        <Col sm={12}>
+                            <span id="Header">Mendez Solutions</span>
                         </Col>
-                        <Col xs={6}>
-                            <button className="HomeButton">Contact</button>
-                        </Col>       
+                    </Row>
+                    <Row>
+                        <Col sm={12}>
+                        <span id="subTitle" >A place where your problems get solved.</span>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={{span:10, offset:0}}>
+                            <Row className="HomeButtons">
+                                <Col xs={6}>
+                                    <button className="HomeButton" >About</button>
+                                </Col>
+                                <Col xs={6  }>
+                                    <button className="HomeButton">Contact</button>
+                                </Col>       
+                            </Row>
+                        </Col>
                     </Row>
                 </Col>
                 <Col md={6} lg={7} xl={8} id="Animation">
