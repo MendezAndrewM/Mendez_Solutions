244aa172aac9a0d1c194d0531e3d164271cde302
Andrew Mendez <mendez.andrewm@gmail.com>
2020-01-24 21:49:12 -0700

    Started Nav Component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,8 +1,31 @@
-import { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 
 import "./Nav.css";
 
-const Nav = props => {
+const Nav = props => (
+	<>
+		<Row>
+			<Col xs={10} sm={9} md={8}>
+				<Link id="navHeader"></Link>
+			</Col>
+			<Col className="justify-content-xs-end">
+				<ul>
+					<li>
+						<Link to="/">About</Link>
+					</li>
+					<li>
+						<Link to="/Profile">Profile</Link>
+					</li>
+					<li>
+						<Link to="/Contact">Contact</Link>
+					</li>
+				</ul>
+			</Col>
+		</Row>
+	</>
+)
+	
 
-}
 export default Nav;
